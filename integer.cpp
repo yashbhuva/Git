836003b5e6f2738bec06183a612fc9009761b008
Yash Bhuva <yash.workmail09@gmail.com>
2021-09-29 18:31:07 +0530

    initial commit

---
+++ integer.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int glo = 6;
+void sum()
+{
+    int a;
+    cout<<glo;
+}
+int main()
+{
+    int glo=25;
+    glo=6170;
+
+    int a=10, b=20;
+    float pi=3.14;
+    char p='y';
+    sum();
+    cout<<glo;
+    
+    //cout<<"This is output of a="<<a<<". \nthis is output of b="<< b;
+    //cout<<"\nThe Value of PI is: "<<pi;
+    //cout<<"\nThe Value of P is: "<<p;
+    return 0;
+}
